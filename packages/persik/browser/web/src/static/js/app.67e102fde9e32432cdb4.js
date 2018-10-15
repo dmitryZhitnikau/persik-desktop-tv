d044ebd625ba6f03a67f19295e65dfab9478e44b
Dmitry Zhitnikov <dmi3z@yandex.ru>
2018-10-15 09:29:29 +0300

    ya metric

--- packages/persik/browser/web/src/static/js/app.67e102fde9e32432cdb4.js
+++ packages/persik/browser/web/src/static/js/app.67e102fde9e32432cdb4.js
@@ -14495,7 +14495,7 @@ exports.default = {
     getInfo: function getInfo() {
       this.getDeviceInfo();
       this.getUuid();
-      var env = __webpack_require__.i({"NODE_ENV":"production","BUILD_DATE":"201810121627","BUILD_NUMBER":'N/A'});
+      var env = __webpack_require__.i({"NODE_ENV":"production","BUILD_DATE":"201810150926","BUILD_NUMBER":'N/A'});
       var buildNumber = env.BUILD_NUMBER ? '#' + env.BUILD_NUMBER : 'N/A';
       this.info.build = buildNumber + ' (' + env.BUILD_DATE + ')';
       this.info.resolution = this.device.display.width + 'x' + this.device.display.height;
@@ -21207,7 +21207,7 @@ exports.default = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.de
         case 9:
           console.log('Backend', backend.code, 'inited');
 
-          env = __webpack_require__.i({"NODE_ENV":"production","BUILD_DATE":"201810121627","BUILD_NUMBER":'N/A'});
+          env = __webpack_require__.i({"NODE_ENV":"production","BUILD_DATE":"201810150926","BUILD_NUMBER":'N/A'});
           appVersion = env.BUILD_NUMBER ? '#' + env.BUILD_NUMBER + ' (' + env.BUILD_DATE + ')' : 'N/A';
           metric = _Metric2.default.getInstance();
 
--- packages/persik/browser/web/src/static/js/manifest.9d72c3c273b5e0919cfd.js
+++ packages/persik/browser/web/src/static/js/manifest.9d72c3c273b5e0919cfd.js
@@ -91,7 +91,7 @@
 /******/ 		if (__webpack_require__.nc) {
 /******/ 			script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 		}
-/******/ 		script.src = __webpack_require__.p + "static/js/" + chunkId + "." + {"0":"dfac397ad60c20400aa3","1":"f9060f2d3c6f16f9244b"}[chunkId] + ".js";
+/******/ 		script.src = __webpack_require__.p + "static/js/" + chunkId + "." + {"0":"dfac397ad60c20400aa3","1":"67e102fde9e32432cdb4"}[chunkId] + ".js";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {
